5599c2a6d124076e82d7c70a9e566a9abb9cbfab
Otamusan <tamumu.naka@gmail.com>
2017-12-21 23:01:45 +0900

    ritual change

---
+++ src/Blocks/BlockRitualStone.java
@@ -0,0 +1,12 @@
+package Blocks;
+
+import Rituals.RitualStones;
+import net.minecraft.block.material.Material;
+
+public class BlockRitualStone extends BlockRitual {
+
+	public BlockRitualStone(Material materialIn, int particleAmount, RitualStones tier) {
+		super(materialIn, particleAmount, tier);
+	}
+
+}
