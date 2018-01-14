fd42a2ce3229b8083324a09a3b5011e8e7a77da8
Otamusan <tamumu.naka@gmail.com>
2018-01-14 22:45:59 +0900

    capturedDropsの挙動が分からないやつ

--- src/Item/Tools/ItemAdvancedExperienceIronSword.java
+++ src/Item/Tools/ItemAdvancedExperienceIronSword.java
@@ -1,13 +1,22 @@
 package Item.Tools;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 import Util.ParticleUtil;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -38,17 +47,58 @@ public class ItemAdvancedExperienceIronSword extends ItemSword {
 		ParticleUtil.blockSurface(EnumParticleTypes.FIREWORKS_SPARK, world, pos, 10);
 		ParticleUtil.blockRemaining(EnumParticleTypes.FIREWORKS_SPARK, world, pos, 10);
 
-		if (!world.isRemote && state.getBlockHardness(world, pos) != 0.0D) {
-			stack.damageItem(1, entityLiving);
-		}
 		return false;
 	}
 
-	@Override
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
-		ParticleUtil.verticalCircle(EnumParticleTypes.FIREWORKS_SPARK, entity.getEntityWorld(), entity.posX,
-				entity.posY + entity.getMaxFallHeight() / 2, entity.posZ, 1, 12);
+		/*
+		 * Entity newentity = null; try { newentity =
+		 * entity.getClass().getConstructor(World.class).newInstance(entity.
+		 * worldObj); } catch (InstantiationException | IllegalAccessException |
+		 * IllegalArgumentException | InvocationTargetException |
+		 * NoSuchMethodException | SecurityException e) { e.printStackTrace(); }
+		 * if (newentity == null) return false;
+		 * entity.worldObj.loadedEntityList.add(newentity);
+		 * newentity.setPosition(entity.posX, entity.posY, entity.posZ);
+		 * entity.onKillCommand();
+		 * 
+		 * System.out.println(player.worldObj.isRemote);
+		 */
+		/*
+		 * if (!(entity instanceof EntityLivingBase)) return false;
+		 * EntityLivingBase living = (EntityLivingBase) entity;
+		 * ArrayList<EntityItem> list = (ArrayList<EntityItem>)
+		 * (living.capturedDrops.clone()); int i =
+		 * net.minecraftforge.common.ForgeHooks.getLootingLevel(entity, player,
+		 * new EntityDamageSource("player", player));
+		 * 
+		 * net.minecraftforge.common.ForgeHooks.onLivingDrops(living, new
+		 * EntityDamageSource("player", player), list, i, true);
+		 */
+		if (!(entity instanceof EntityLivingBase))
+			return false;
+		EntityLivingBase living = (EntityLivingBase) entity;
+		EntityDamageSource cause = new EntityDamageSource("player", player);
+		System.out.println(living.capturedDrops);
+		living.onDeath(cause);
+		System.out.println(living.capturedDrops);
+
+		ArrayList<EntityItem> list = null;
+		try {
+			list = deepcopy(living.capturedDrops);
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		living.capturedDrops = list;
+		living.isDead = false;
 		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T deepcopy(T obj) throws IOException, ClassNotFoundException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		new ObjectOutputStream(baos).writeObject(obj);
+		return (T) new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray())).readObject();
 	}
 
 	@Override
