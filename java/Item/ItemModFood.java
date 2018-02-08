595c0198c30ec538a1ceb71ac84b55929c9f09e9
Otamusan <tamumu.naka@gmail.com>
2018-02-08 23:11:52 +0900

    食料を食べる速度を変更できるように

--- java/Item/ItemModFood.java
+++ java/Item/ItemModFood.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 import ExperienceApple.ITooltip;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 
 public class ItemModFood extends ItemFood implements ITooltip {
 
-	public ItemModFood(int amount, float saturation, boolean isWolfFood) {
+	private int speed;
+
+	public ItemModFood(int amount, float saturation, boolean isWolfFood, int speed) {
 		super(amount, saturation, isWolfFood);
+		this.speed = speed;
 	}
 
 	private List<String> Tooltip = new ArrayList<String>();
@@ -17,6 +21,10 @@ public class ItemModFood extends ItemFood implements ITooltip {
 	@Override
 	public List<String> getTooltip() {
 		return Tooltip;
+	}
+
+	public int getMaxItemUseDuration(ItemStack stack) {
+		return 4;
 	}
 
 	@Override
